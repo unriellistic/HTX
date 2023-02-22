890411b420638874f68e85eb30a51a5dbd37a478
Alp <ang096@e.ntu.edu.sg>
2023-02-22 10:23:26 +0800

    added in siamese stuff

---
+++ siamese/siamesenetwork.py
@@ -0,0 +1,169 @@
+from __future__ import print_function
+import argparse, random, copy
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision import transforms as T
+from torch.optim.lr_scheduler import StepLR
+
+torch.cuda.is_available()
+
+#create a siamese network
+class SiameseNetwork(nn.Module):
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        # Setting up the Sequential of CNN Layers
+        self.cnn1 = nn.Sequential(
+            nn.Conv2d(1, 96, kernel_size=11,stride=1),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
+            nn.MaxPool2d(3, stride=2),
+            
+            nn.Conv2d(96, 256, kernel_size=5,stride=1,padding=2),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+
+            nn.Conv2d(256,384 , kernel_size=3,stride=1,padding=1),
+            nn.ReLU(inplace=True),
+            
+            nn.Conv2d(384,256 , kernel_size=3,stride=1,padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+        )
+        # Defining the fully connected layers
+        self.fc1 = nn.Sequential(
+            nn.Linear(30976, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5),
+            
+            nn.Linear(1024, 128),
+            nn.ReLU(inplace=True),
+            
+            nn.Linear(128,2))
+        
+    def forward_once(self, x):
+        # Forward pass 
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2):
+        # forward pass of input 1
+        output1 = self.forward_once(input1)
+        # forward pass of input 2u
+        output2 = self.forward_once(input2)
+        return output1, output2
+
+class ContrastiveLoss(torch.nn.Module):
+    """
+    Contrastive loss function.
+    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    """
+
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
+                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+
+
+        return loss_contrastive
+
+class SiameseNetworkDataset(Dataset):
+    
+    def __init__(self,imageFolderDataset,transform=None,should_invert=True):
+        self.imageFolderDataset = imageFolderDataset
+
+        self.transform = transform
+        self.should_invert = should_invert
+        
+    def __getitem__(self,index):
+        img0_tuple = random.choice(self.imageFolderDataset.imgs)
+        #we need to make sure approx 50% of images are in the same class
+        should_get_same_class = random.randint(0,1) 
+        if should_get_same_class:
+            while True:
+                #keep looping till the same class image is found
+                img1_tuple = random.choice(self.imageFolderDataset.imgs) 
+                if img0_tuple[1]==img1_tuple[1]:
+                    break
+        else:
+            img1_tuple = random.choice(self.imageFolderDataset.imgs)
+
+        img0 = Image.open(img0_tuple[0])
+        img1 = Image.open(img1_tuple[0])
+        img0 = img0.convert("L")
+        img1 = img1.convert("L")
+        
+        if self.should_invert:
+            img0 = PIL.ImageOps.invert(img0)
+            img1 = PIL.ImageOps.invert(img1)
+
+        if self.transform is not None:
+            img0 = self.transform(img0)
+            img1 = self.transform(img1)
+        
+        return img0, img1 , torch.from_numpy(np.array([int(img1_tuple[1]!=img0_tuple[1])],dtype=np.float32))
+    
+    def __len__(self):
+        return len(self.imageFolderDataset.imgs)
+
+net = SiameseNetwork().cuda()
+criterion = ContrastiveLoss()
+optimizer = optim.Adam(net.parameters(),lr = 0.0005 )
+
+counter = []
+loss_history = [] 
+iteration_number= 0
+
+for epoch in range(0,Config.train_number_epochs):
+    for i, data in enumerate(train_dataloader,0):
+        img0, img1 , label = data
+        img0, img1 , label = Variable(img0).cuda(), Variable(img1).cuda() , Variable(label).cuda()
+        output1,output2 = net(img0,img1)
+        optimizer.zero_grad()
+        loss_contrastive = criterion(output1,output2,label)
+        loss_contrastive.backward()
+        optimizer.step()
+        if i %10 == 0 :
+            print("Epoch number {}\n Current loss {}\n".format(epoch,loss_contrastive.data[0]))
+            iteration_number +=10
+            counter.append(iteration_number)
+            loss_history.append(loss_contrastive.data[0])
+show_plot(counter,loss_history)
+
+folder_dataset_test = dset.ImageFolder(root=Config.testing_dir)
+siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset_test,
+                                        transform=transforms.Compose([transforms.Scale((100,100)),
+                                                                      transforms.ToTensor()
+                                                                      ])
+                                       ,should_invert=False)
+
+test_dataloader = DataLoader(siamese_dataset,num_workers=6,batch_size=1,shuffle=True)
+dataiter = iter(test_dataloader)
+x0,_,_ = next(dataiter)
+
+for i in range(10):
+    _,x1,label2 = next(dataiter)
+    concatenated = torch.cat((x0,x1),0)
+    
+    output1,output2 = net(Variable(x0).cuda(),Variable(x1).cuda())
+    euclidean_distance = F.pairwise_distance(output1, output2)
+    imshow(torchvision.utils.make_grid(concatenated),'Dissimilarity: {:.2f}'.format(euclidean_distance.cpu().data.numpy()[0][0]))
+
+
+
+
