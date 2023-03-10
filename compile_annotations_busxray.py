7b8b9a8cfbd24a7df26eff172a96ae13917cb447
Alp <ang096@e.ntu.edu.sg>
2023-03-10 14:56:56 +0800

    a simple script that compiles all the xml and jpg annotations by renaming them into their respective folder number and storing them into the 'annotation' folder

---
+++ compile_annotations_busxray.py
@@ -0,0 +1,31 @@
+"""
+This scripts goes through each folder in the "exp" folder and compiles the files with "annotated" (both .xml and .jpg) and copies 
+them into a folder called "annotated", and relabels them according to their annotations.
+"""
+
+import os
+import shutil
+
+# Constants to change for differing paths
+ROOT_DIR = r"D:\leann\busxray_woodlands\exp"
+ANNOTATION_DIR = r"D:\leann\busxray_woodlands\annotations"
+
+def listdirs(ROOT_DIR):
+    for root, dirs, _ in os.walk(ROOT_DIR):
+
+        # Go through the list of subdirectories
+        for subdir in dirs:
+         
+            # Go through each file in the list
+            for file in os.listdir(os.path.join(root, subdir)):
+        
+                # Check if it's the annotation file
+                if "annotated.xml" in file:
+                    _, file_type = os.path.splitext(file)
+                    old_name = os.path.join(os.path.abspath(root), os.path.join(subdir, file))
+                    new_name = os.path.join(ANNOTATION_DIR, f"{subdir}_annotated{file_type}")
+                    shutil.copy(old_name, new_name)
+                    print(subdir)
+
+if __name__ == "__main__":
+    listdirs(ROOT_DIR)
