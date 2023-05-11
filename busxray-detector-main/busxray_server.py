a8e1ceba1d669e48c7102d6c2750e16b7b14489e
Alp <ang096@e.ntu.edu.sg>
2023-05-11 17:32:14 +0800

    developing inference code

--- busxray-detector-main/busxray_server.py
+++ busxray-detector-main/busxray_server.py
@@ -9,8 +9,7 @@ from sanic.response import json
 from sanic.worker.loader import AppLoader
 
 from tridentnet_predictor import TridentNetPredictor
-import real_time_bus_segmenting_script as rtbss
-
+import inference as inf
 
 # change this if you want to use a different model
 # predictor should be callable, take openCV-format image as input, and output JSON-compatible predictions
@@ -18,112 +17,7 @@ predictor = TridentNetPredictor(config_file="models/tridentnet_fast_R_50_C4_3x.y
     opts=["MODEL.WEIGHTS", "models/model_final_e1027c.pkl"]
 )
 
-def calculate_iou(box1, box2):
-    """
-    Calculate IoU (Intersection over Union) between two bounding boxes.
 
-    Args:
-        box1: Tuple or list representing the first bounding box [{  'bbox': [xmin, ymin, width, height],
-                                                                    'score': score,
-                                                                    'pred_class': id}].
-        box2: Tuple or list representing the second bounding box (x_min, y_min, x_max, y_max).
-
-    Returns:
-        IoU value.
-    """
-    # reformat box to fit existing code that works
-    box_1_x_min = box1['bbox'][0]
-    box_1_y_min = box1['bbox'][1]
-    box_1_x_max = box_1_x_min + box1['bbox'][2]
-    box_1_y_max = box_1_y_min + box1['bbox'][3]
-    box_2_x_min = box2['bbox'][0]
-    box_2_y_min = box2['bbox'][1]
-    box_2_x_max = box_2_x_min + box2['bbox'][2]
-    box_2_y_max = box_2_y_min + box2['bbox'][3]
-
-    # Existing code that works
-    x1 = max(box_1_x_min, box_2_x_min)
-    y1 = max(box_1_y_min, box_2_y_min)
-    x2 = min(box_1_x_max, box_2_x_max)
-    y2 = min(box_1_y_max, box_2_y_max)
-
-    intersection = max(0, x2 - x1 + 1) * max(0, y2 - y1 + 1)
-
-    box1_area = (box_1_x_max - box_1_x_min + 1) * (box_1_y_max - box_1_y_min + 1)
-    box2_area = (box_2_x_max - box_2_x_min + 1) * (box_2_y_max - box_2_y_min + 1)
-
-    union = box1_area + box2_area - intersection
-
-    iou = intersection / union
-
-    return iou
-
-def custom_nms(bounding_boxes, iou_threshold):
-    """
-    Perform a customized non-maximal suppression (NMS)-like algorithm to merge overlapping bounding boxes.
-
-    Args:
-        bounding_boxes: List of bounding boxes [(x_min, y_min, x_max, y_max, confidence, class_id)].
-        iou_threshold: IoU threshold to determine overlap.
-
-    Returns:
-        List of non-overlapping bounding boxes.
-    """
-    sorted_boxes = sorted(bounding_boxes, key=lambda x: x["score"], reverse=True) # Sort by confidence
-
-    selected_boxes = []
-    while sorted_boxes:
-        current_box = sorted_boxes.pop(0)
-        selected_boxes.append(current_box)
-
-        remaining_boxes = []
-        for box in sorted_boxes:
-            iou = calculate_iou(current_box, box)
-            if iou <= iou_threshold:
-                remaining_boxes.append(box)
-
-        sorted_boxes = remaining_boxes
-
-    return selected_boxes
-
-# Get image
-img_path = Path(r"C:\Users\User1\Desktop\alp\busxray-detector-main\PA8506K Higer 49 seats-Threat-3-final_color.jpg")
-logger.info("Processing image " + str(img_path))
-start_time = time.perf_counter()
-img_cv2 = cv2.imread(str(img_path))
-
-# Crop and segment image
-_, segment_images_info = rtbss.calling_relevant_class_function(cv2_image=img_cv2, segment_size=640, overlap_percent=0.5)
-
-# Initialise empty list of bounding_boxes
-bounding_boxes = []
-
-# Run on every segment
-for segment, segment_info in tqdm(segment_images_info.items()):
-
-    # Get original coordinates from segment name
-    x_offset = int(segment.split("_")[2])
-    y_offset = int(segment.split("_")[1])
-
-    # run AI prediction
-    predictions = predictor(segment_info) # should be COCO format (json compatible)
-
-    # If prediction was made
-    if predictions:
-    
-        # if predictions has multiple annotations return, iterate through each one
-        for prediction in predictions:
-            # calculate original coordinates
-            prediction["bbox"][0] = prediction["bbox"][0] + x_offset
-            prediction["bbox"][1] = prediction["bbox"][1] + y_offset
-
-            # Update list of bounding_boxes -> Contains a list of original adjusted [xmin,xmax,ymin,ymax,confidence]
-            bounding_boxes.append(prediction)
-
-# Perform NMS here. Limitations: We only stitch together same class id prediction together
-# Assuming you have a list of bounding boxes: [(bbox, score, pred_class)]
-IOU_THRESHOLD = 0.5
-non_overlapping_boxes = custom_nms(bounding_boxes, IOU_THRESHOLD)
 
 
 # save the prediction to json file (not needed as this is done on client side)
--- test.py
+++ test.py
@@ -1,112 +1,19 @@
 """
 Just a mini testing file for me to try out python logic for debugging.
 """
-
-data = {
-  "predictions": [
-    {
-      "class": "person",
-      "confidence": 0.92,
-      "bounding_box": {
-        "x": 100,
-        "y": 200,
-        "width": 300,
-        "height": 400
-      }
-    },
-    {
-      "class": "car",
-      "confidence": 0.85,
-      "bounding_box": {
-        "x": 400,
-        "y": 150,
-        "width": 200,
-        "height": 250
-      }
-    },
-    ...
-  ]
-}
-
-# Run on every segment
-
-# Initialise empty list of bounding_boxes
-bounding_boxes = []
-  # Know which segment we're running prediction on
-  # run prediction
-  # calculate original position
-  # Update list of bounding_boxes -> Contains a list of original adjusted [xmin,xmax,ymin,ymax,confidence]
-
-# Sort it according to confidence score
-sorted_boxes = sorted(bounding_boxes, key=lambda x: x[4], reverse=True)  # Sort by confidence
-# Perform NMS here. Limitations: We only stitch together same class id prediction together
-
-
-
-# # If current location has another label, perform non-maximal suppression. (What if there are 2 distinct items in overlap? Maybe a gun overlap with a cig?)
-
-def calculate_iou(box1, box2):
-    """
-    Calculate IoU (Intersection over Union) between two bounding boxes.
-
-    Args:
-        box1: Tuple or list representing the first bounding box (x_min, y_min, x_max, y_max).
-        box2: Tuple or list representing the second bounding box (x_min, y_min, x_max, y_max).
-
-    Returns:
-        IoU value.
-    """
-    x1 = max(box1[0], box2[0])
-    y1 = max(box1[1], box2[1])
-    x2 = min(box1[2], box2[2])
-    y2 = min(box1[3], box2[3])
-
-    intersection = max(0, x2 - x1 + 1) * max(0, y2 - y1 + 1)
-
-    box1_area = (box1[2] - box1[0] + 1) * (box1[3] - box1[1] + 1)
-    box2_area = (box2[2] - box2[0] + 1) * (box2[3] - box2[1] + 1)
-
-    union = box1_area + box2_area - intersection
-
-    iou = intersection / union
-
-    return iou
-
-def custom_nms(bounding_boxes, iou_threshold):
-    """
-    Perform a customized non-maximal suppression (NMS)-like algorithm to merge overlapping bounding boxes.
-
-    Args:
-        bounding_boxes: List of bounding boxes [(x_min, y_min, x_max, y_max, confidence, class_id)].
-        iou_threshold: IoU threshold to determine overlap.
-
-    Returns:
-        List of non-overlapping bounding boxes.
-    """
-    sorted_boxes = sorted(bounding_boxes, key=lambda x: x[4], reverse=True)  # Sort by confidence
-
-    selected_boxes = []
-    while sorted_boxes:
-        current_box = sorted_boxes.pop(0)
-        selected_boxes.append(current_box)
-
-        remaining_boxes = []
-        for box in sorted_boxes:
-            iou = calculate_iou(current_box, box)
-            if iou <= iou_threshold:
-                remaining_boxes.append(box)
-
-        sorted_boxes = remaining_boxes
-
-    return selected_boxes
-
-# Assuming you have a list of bounding boxes: [(x_min, y_min, x_max, y_max, confidence, class_id)]
-bounding_boxes = 0.5
-non_overlapping_boxes = custom_nms(bounding_boxes, iou_threshold)
-
-# Print the resulting non-overlapping bounding boxes
-for box in non_overlapping_boxes:
-    print(box) = [(10, 20, 50, 60, 0.9, 1), (30, 40, 70, 80, 0.8, 2), (40, 55, 80, 90, 0.9, 3), (15, 20, 80, 90, 0.7, 3)]
-
-# Call the custom NMS function
-iou_threshold
+import numpy as np
+height = 1000
+segment_size = 2000
+# Check if segment size specified is within height/width of image
+if segment_size > height:
+    print(f"Segment size larger than image's height. Changing segment_size to be {height}")
+    segment_size = height
+elif segment_size > width:
+    print(f"Segment size larger than image's width. Changing segment_size to be {width}")
+    segment_size = width
+overlap_pixels = int(segment_size * 0.5)
+segment_stride = segment_size - overlap_pixels
+print("(height - segment_size)/ segment_stride:", (height - segment_size)/ segment_stride)
+print("np.ceil((height - segment_size) / segment_stride:", np.ceil((height - segment_size) / segment_stride))
+print(int(np.ceil((height - segment_size) / segment_stride)))
+num_rows = int(np.ceil((height - segment_size) / segment_stride)) + 1
