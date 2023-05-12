0c2531aef57e2e10228bdd2d19f6f340f2d93a3a
Alp <ang096@e.ntu.edu.sg>
2023-05-12 15:31:56 +0800

    refactored code to make it more efficient

--- cleanup_busxray_script/compile_annotations_busxray.py
+++ cleanup_busxray_script/compile_annotations_busxray.py
@@ -1,5 +1,6 @@
 """
 This script works only for the original "exp" directory-structure.
+Note: the .xml annotation must have the same name as the image file
 
 It does 2 function:
 1) It goes through each folder in the "exp" folder and looks for the files with "annotated" (both .xml and .jpg) and copies 
@@ -8,8 +9,8 @@ them over into a folder called "annotated".
 E.g. if image and xml were saved in subdirectory named 355, it'll re-save the image and xml as 355_annotated.jpg and 355_annotated.xml
 
 To alter the script to compile different files,
-look at line 43 and update the <if "annotated" in file:> to contain whatever unique description the file name has, 
-else if you want to compile all the files in the subdirectory, uncomment that line out, and alt-tab the function.
+look at line 35 and update the <if "annotated" in file:> to contain whatever unique description the file name has, 
+else if you want to compile all the files in the subdirectory, delete that portion.
 
 Input Arguments:
 --root-dir: specifies the folder that contains a subdirectories of image and XML.
@@ -22,36 +23,41 @@ python compile_annotations_busxray.py --root-dir "D:\leann\busxray_woodlands\exp
 
 This will cause the function to look at root directory at <exp> and saves the file at <annotations>.
 @current_author: alp
-@last modified: 12/4/2023 2:29pm
+@last modified: 12/5/2023 3:29pm
 """
 
-import os, shutil, argparse
+import os, shutil, argparse, general_scripts as gs
+from tqdm import tqdm
 
 def compile_annotations(ROOT_DIR, ANNOTATION_DIR):
     
     # Create the output directory if it does not exist
-    if not os.path.exists(ANNOTATION_DIR):
-        os.makedirs(ANNOTATION_DIR)
-
-    for root, dirs, _ in os.walk(ROOT_DIR):
-
-        # Go through the list of subdirectories
-        for subdir in dirs:
-         
-            # Go through each file in the list
-            for file in os.listdir(os.path.join(root, subdir)):
-        
-                # Check if it's the annotation file
-                if "annotated" in file:
-                    _, file_type = os.path.splitext(file)
-                    old_name = os.path.join(os.path.abspath(root), os.path.join(subdir, file))
-                    new_name = os.path.join(ANNOTATION_DIR, f"{subdir}_annotated{file_type}")
-                    shutil.copy(old_name, new_name)
+    os.makedirs(ANNOTATION_DIR, exist_ok=True)
+    files = [file for file in gs.load_images(ROOT_DIR, recursive=True) if "annotated" in file]
+    print(f"Copying over from {ROOT_DIR} ...")
+    for file in tqdm(files):
+        file_path, file_name = gs.path_leaf(file)
+        _, subdir_name = gs.path_leaf(file_path)
+        _, file_type = os.path.splitext(file_name)
+        new_name = os.path.join(ANNOTATION_DIR, f"{subdir_name}_annotated{file_type}")
+        shutil.copy(file, new_name)
+        shutil.copy(file, gs.change_file_extension(new_name, ".xml"))
+        print(f"Copied over '{file}' to '{ANNOTATION_DIR}'")                   
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--root-dir", help="folder containing the X-ray images", default=r"exp")
     parser.add_argument("--target-dir", help="path to folder to store consolidated image + annotation files", default=r"compiled_annotations")
+    args = parser.parse_args() 
+    """
+    Just to clear some bug that occurs if target-dir default options settings is left default.
+    """
+    # Get path to root directory folder
+    # Check if default parameter is applied, if so get full path.
+    if args.target_dir == "compiled_annotations":
+        path_to_target_dir = os.path.join(os.getcwd(), args.target_dir)
+    # Else, use path specified by user
+    else:
+        path_to_target_dir = args.target_dir
 
-    args = parser.parse_args()
-    compile_annotations(args.root_dir, args.target_dir)
+    compile_annotations(args.root_dir, path_to_target_dir)
