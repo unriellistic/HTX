67b4dbd208f246a365e856ddc9f147df40deae50
Alphaeus Ng <ang096@e.ntu.edu.sg>
2023-05-29 17:01:23 +0800

    removed convert_and_organise_files.py

--- cleanup_busxray_script/convert_and_organise_files.py
+++ cleanup_busxray_script/convert_and_organise_files.py
@@ -2,13 +2,13 @@
 Combines xml2yolo.py, consolidate_segmented_files.py, and split_train_test_val.py.
 """
 import xml2yolo as x2y
-import consolidate_segmented_files as csf
 import split_train_test_val as sttv
 import argparse, general_scripts as gs, os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--root-dir", help="directory to the image and annotation files", default=r"annotations_adjusted")
+    parser.add_argument("--root-dir", help="directory to the image and annotation files",
+                        default=r"annotations_adjusted")
     parser.add_argument("--train", help="value for train folder split", default=0.8)
     parser.add_argument("--test", help="value for test folder split", default=0.1)
     parser.add_argument("--valid", help="value for validation folder split", default=0.1)
@@ -17,11 +17,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # Change this to the folder that contains all the images and xml files. 
     basepath, pathname = gs.path_leaf(args.root_dir)
-    target_dir = os.path.join(basepath, f"segmented_{pathname}")
     output_folder = os.path.join(basepath, f"output_{pathname}")
 
     # The class
     CLASSES = ["cig", "guns", "human", "knives", "drugs", "exp"]
-    x2y.convert_xml_to_yolo(args.root_dir, classes=CLASSES)
-    # csf.consolidate_files(args.root_dir, target_dir)
-    sttv.split_data(input_folder=target_dir, output_folder=output_folder, train_ratio=args.train, test_ratio=args.test, val_ratio=args.valid, seed=args.seed)
+    x2y.convert_xml_to_yolo(args.root_dir, classes=CLASSES) # remember to uncomment
+    sttv.split_data(input_folder=args.root_dir, output_folder=output_folder, train_ratio=args.train,
+                    test_ratio=args.test, val_ratio=args.valid, seed=args.seed)
