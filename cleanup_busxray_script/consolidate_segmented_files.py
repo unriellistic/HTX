e0670fe0dcf8fc2ca47cf5546ffa340b61b3682b
Alphaeus Ng <ang096@e.ntu.edu.sg>
2023-05-29 15:42:07 +0800

    commented out the consolidate files function. split_train_test_val.py will automatically consolidate from annotation_adjusted directory

--- cleanup_busxray_script/consolidate_segmented_files.py
+++ cleanup_busxray_script/consolidate_segmented_files.py
@@ -18,13 +18,14 @@ def consolidate_files(ROOT_DIR, TARGET_DIR):
         segmented_filepath, segmented_filename = gs.path_leaf(segmented_file)
         _, segmented_filepath_name = gs.path_leaf(segmented_filepath)
 
-        # Get rid of adjusted and segmented at front and back of file name
-        segmented_filepath_name = segmented_filepath_name[9:-10]
+        # Get rid of segmented at back of file name
+        segmented_filepath_name = segmented_filepath_name[:-10]
 
         # src and dst path for image
         src_path = segmented_file
         dst_path = os.path.join(TARGET_DIR, f"{segmented_filepath_name}_{segmented_filename}")
         shutil.copy(src_path, dst_path)
+
         # src and dst path for label
         src_path = os.path.join(gs.change_file_extension(segmented_file, ".txt"))
         if os.path.exists(src_path):
@@ -38,7 +39,7 @@ def consolidate_files(ROOT_DIR, TARGET_DIR):
 
 if __name__ == "__main__":
     print("This script is not meant to be run directly.")
-    print("Please import it as a module and call the consolidate_files() function, unless you're debuging.")
+    print("Please import it as a module and call the consolidate_files() function, unless you're debugging.")
     ROOT_DIR = r"D:\BusXray\scanbus_training\adjusted_master_file_for_both_clean_and_threat_images_monochrome"
     TARGET_DIR = r"D:\BusXray\scanbus_training\segmented_master_file_for_both_clean_and_threat_images_monochrome"
     consolidate_files(ROOT_DIR, TARGET_DIR)
