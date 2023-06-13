af679f27fc346e550a5b0f1601faf81248d8eb26
unriellistic <charliewindle@gmail.com>
2023-06-13 16:45:11 +0800

    overhauled yolo annotation in draw_annotation.py using code from inference.py

--- cleanup_busxray_script/draw_annotation.py
+++ cleanup_busxray_script/draw_annotation.py
@@ -5,53 +5,42 @@ import argparse
 from PIL import Image, ImageDraw
 import cv2, os, random
 
+CLASS_NAMES = ["cig", "guns", "human", "knives", "drugs", "exp"]
+
 """
 To check YOLO formatted annotations (.txt files)
 """
-def plot_one_box(x, image, color=None, label=None, line_thickness=None):
-    # Plots one bounding box on image img
-    tl = line_thickness or round(
-        0.002 * (image.shape[0] + image.shape[1]) / 2) + 1  # line/font thickness
-    color = color or [random.randint(0, 255) for _ in range(3)]
-    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(image, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
-    if label:
-        tf = max(tl - 1, 1)  # font thickness
-        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
-        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-        cv2.rectangle(image, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(image, label, (c1[0], c1[1] - 2), 0, tl / 3,
-                    [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+def draw_yolo_annotations(image_path: str, label_path: str, save_path: str = None, display: bool = True) -> None:
+    annotated_image = cv2.imread(image_path)
+    with open(label_path, "r") as f:
+        for line in f:
+            line = line.split()
+            class_name = CLASS_NAMES[int(line[0])]
 
-def draw_YOLO_annotations(image_path, txt_path, save_path):
-    """
-    This function will add rectangle boxes on the images.
-    """
-    # flag_people_or_car_data = 0  #变量 代表类别
-    source_file = open(txt_path) if os.path.exists(txt_path) else []
-    image = cv2.imread(image_path)
-    try:
-        height, width, channels = image.shape
-    except:
-        print('no shape info.')
-        return 0
+            # YOLO format: xmid, ymid, w, h
+            x, y, w, h = map(float, line[1:])
 
-    box_number = 0
-    for line in source_file:  # 例遍 txt文件得每一行
-        staff = line.split()  # 对每行内容 通过以空格为分隔符对字符串进行切片
+            # Convert to x1, y1, x2, y2
+            xmin = int((x - w / 2) * annotated_image.shape[1])
+            ymin = int((y - h / 2) * annotated_image.shape[0])
+            xmax = int((x + w / 2) * annotated_image.shape[1])
+            ymax = int((y + h / 2) * annotated_image.shape[0])
 
-        x_center, y_center, w, h = float(
-            staff[1])*width, float(staff[2])*height, float(staff[3])*width, float(staff[4])*height
-        x1 = round(x_center-w/2)
-        y1 = round(y_center-h/2)
-        x2 = round(x_center+w/2)
-        y2 = round(y_center+h/2)
+            # Draw the bounding box
+            cv2.rectangle(annotated_image, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 0, 255), 4)
+            cv2.putText(annotated_image, class_name, (int(xmin)-4, int(ymin)-14), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
 
-        plot_one_box([x1, y1, x2, y2], image, line_thickness=None)
+    # save the image
+    if save_path:
+        cv2.imwrite(save_path, annotated_image)
 
-        cv2.imwrite(save_path, image)
-        box_number += 1
-    return box_number
+    # Display the annotated image
+    if display:
+        cv2.namedWindow('Annotated Image', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Annotated Image', annotated_image.shape[1], annotated_image.shape[0])
+        cv2.imshow("Annotated Image", annotated_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 """
 To check Pascal VOC images (.xml files)
@@ -102,6 +91,6 @@ if __name__ == "__main__":
         raise ValueError("Format must be either 'yolo' or 'voc'")
     
     if args.format == "yolo":
-        draw_YOLO_annotations(args.image_path, args.label_path, args.output)
+        draw_yolo_annotations(args.image_path, args.label_path, args.output)
     elif args.format == "voc":
         draw_xml_annotations(args.image_path, args.label_path, args.output)
